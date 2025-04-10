6cd4a297302ab9a9682f2ec524f0549ad898afaa
devCaiodias <caiodiasmartins12@gmail.com>
2025-04-10 18:31:44 -0300

    listei todos os dados do customers e também listei por id

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const express = require("express")
 const service = express()
 
-// Query params = ?nome=Caio&idade=17
-service.get("/hello", (req, res) => {
-    const {nome, idade} = req.query
-    res.json({
-        message: "Helloo word  query params",
-        nome,
-        idade
-    })
+service.use(express.json())
+
+let customers = [
+    { id: 1, noma: "Caio", site: "https://devcaiodias.github.io/DevCaiodias-portfolio/",},
+    { id: 2, noma: "Google", site: "https://google.com",},
+    { id: 3, noma: "Uol", site: "https://uol.com.br",}
+]
+
+service.get("/customers", (req, res) => {
+    return res.json(customers)
 })
 
-// Route params = /hello/:nome
+service.get("/customers/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const customer = customers.find(item => item.id === id)
+    const status = customer ? 200 : 404
 
-service.get("/hello/:nome", (req, res) => {
-    const nome = req.params.nome
-
-    res.json({
-        message: "Helloo word Route params",
-        nome
-    })
-})
+    res.status(status).json(customer)
+1})
 
 service.listen(3000)
