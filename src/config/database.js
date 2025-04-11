0234af209cf7512506f0bac2a3a7720d76492638
devCaiodias <caiodiasmartins12@gmail.com>
2025-04-11 18:32:27 -0300

    configurando o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '28292425',
+    database: 'customers',
+    define: {
+        timestamp: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+}
