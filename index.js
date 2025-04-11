0fc4475189de0462d3ff231c975c97ad0a6e772c
devCaiodias <caiodiasmartins12@gmail.com>
2025-04-11 11:23:14 -0300

    fiz o metado  update para alterar os dados

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const service = express()
 service.use(express.json())
 
 let customers = [
-    { id: 1, noma: "Caio", site: "https://devcaiodias.github.io/DevCaiodias-portfolio/",},
-    { id: 2, noma: "Google", site: "https://google.com",},
-    { id: 3, noma: "Uol", site: "https://uol.com.br",}
+    { id: 1, name: "Caio", site: "https://devcaiodias.github.io/DevCaiodias-portfolio/",},
+    { id: 2, name: "Google", site: "https://google.com",},
+    { id: 3, name: "Uol", site: "https://uol.com.br",}
 ]
 
 service.get("/customers", (req, res) => {
@@ -29,6 +29,20 @@ service.post("/customers", (req, res) => {
     customers.push(newCustomer)
 
     return res.status(201).json(newCustomer)
+})
+
+service.put("/customers/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const { name, site} = req.body;
+
+    const index = customers.findIndex(item => item.id === id)
+    const status = index >= 0 ? 200 : 404
+
+    if (index >= 0) {
+        customers[index] = { id: parseInt(id), name, site}
+    }
+
+    return res.status(status).json(customers[index])
 })
 
 service.listen(3000)
