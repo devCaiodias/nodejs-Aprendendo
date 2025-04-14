ead55904439a65a6bb1a0eae1a6c1d0a2dee4b45
devCaiodias <caiodiasmartins12@gmail.com>
2025-04-13 23:47:59 -0300

    Criando mais uma tabela chamada Contact

---
+++ src/database/migrations/20250414022016-create-contact.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    return await queryInterface.createTable('contact', { 
+    id: {
+      type:Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    customer_id: {
+      type: Sequelize.INTEGER,
+      references: {model: 'customers', key: 'id'},
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      allowNull: false
+    }
+  });
+  },
+
+  async down (queryInterface, Sequelize) {
+    return await queryInterface.dropTable('contact');
+  }
+};
